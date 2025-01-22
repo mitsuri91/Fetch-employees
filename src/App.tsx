0567ee3cfa5b4419eff818f09b91e927df434eab
mitsuri91 <launaymaxime5@gmail.com>
2025-01-22 11:02:32 +0100

    fetch api local

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,15 @@
-import { useState } from "react";
-import EmployeeCard from "./components/EmployeeCard";
 import { useEffect } from "react";
 import "./App.css";
 
-const sampleEmployee = {
-	name: {
-		first: "Charlie",
-		last: "Thompson",
-	},
-	email: "charlie.thompson@example.com",
-	picture: {
-		medium: "https://randomuser.me/api/portraits/med/men/40.jpg",
-	},
-};
 function App() {
-	const getEmployee = () => {
-		// Send the request
-		fetch("https://randomuser.me/api?nat=en")
+	useEffect(() => {
+		fetch("http://localhost:3310/api/employees")
 			.then((response) => response.json())
 			.then((data) => {
 				console.log(data);
-				setEmployee(data.results[0]);
 			});
-	};
+	}, []);
 
-	const [employee, setEmployee] = useState(sampleEmployee);
-
-	return (
-		<div className="App">
-			<EmployeeCard employee={employee} />
-			<button type="button" onClick={getEmployee}>
-				Get employee
-			</button>
-		</div>
-	);
+	return <>je suis ici</>;
 }
 export default App;
